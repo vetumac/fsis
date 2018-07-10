7849fc584953c52bf7c1fbb4114aa2b5a2301fd0
Alexei Suslov <alexei.suslov@instinctools.ru>
2018-07-10 09:37:14 +0300

    save Applicative 2

--- build.sbt
+++ build.sbt
@@ -1,5 +1,10 @@
 scalaVersion := "2.12.6"
 
+resolvers ++= Seq(
+  "Sonatype Public" at "https://oss.sonatype.org/content/groups/public/",
+  "bintary/non" at "http://dl.bintary.com/non/maven"
+)
+
 addCompilerPlugin("org.scalamacros" % "paradise" % "2.1.0" cross CrossVersion.full)
 addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.9.7")
 
--- src/main/scala/Applicative.scala
+++ src/main/scala/Applicative.scala
@@ -2,15 +2,44 @@ import simulacrum._
 
 @typeclass
 trait Applicative[F[_]] extends Functor[F] {
+  self =>
   def pure[A](a: A): F[A]
 
   def apply[A, B](fa: F[A])(ff: F[A => B]): F[B]
+
+  def apply2[A, B, Z](fa: F[A], fb: F[B])(ff: F[(A, B) => Z]): F[Z] =
+    apply(fa)(apply(fb)(map(ff)(f => b => a => f(a, b))))
 
   override def map[A, B](fa: F[A])(f: A => B): F[B] =
     apply(fa)(pure(f))
 
   def map2[A, B, Z](fa: F[A], fb: F[B])(f: (A, B) => Z): F[Z] =
     apply(fa)(map(fb)(b => f(_, b)))
+
+  def map3[A, B, C, Z](fa: F[A], fb: F[B], fc: F[C])(f: (A, B, C) => Z): F[Z] =
+    apply(fa)(map2(fb, fc)((b, c) => a => f(a, b, c)))
+
+  def map4[A, B, C, D, Z](fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => Z): F[Z] =
+    map2(tuple2(fa, fb), tuple2(fc, fd)) { case ((a, b), (c, d)) => f(a, b, c, d) }
+
+  def tuple2[A, B](fa: F[A], fb: F[B]): F[(A, B)] =
+    map2(fa, fb)((a, b) => (a, b))
+
+  def tuple3[A, B, C](fa: F[A], fb: F[B], fc: F[C]): F[(A, B, C)] =
+    map3(fa, fb, fc)((a, b, c) => (a, b, c))
+
+  def flip[A, B](ff: F[A => B]): F[A] => F[B] =
+    fa => apply(fa)(ff)
+
+  def compose[G[_]](implicit G: Applicative[G]): Applicative[Lambda[X => F[G[X]]]] =
+    new Applicative[Lambda[X => F[G[X]]]] {
+      override def pure[A](a: A): F[G[A]] = self.pure(G.pure(a))
+
+      override def apply[A, B](fga: F[G[A]])(ff: F[G[A => B]]): F[G[B]] = {
+        val x: F[G[A] => G[B]] = self.map(ff)(gab => G.flip(gab))
+        self.apply(fga)(x)
+      }
+    }
 }
 
 object Applicative {
@@ -31,4 +60,23 @@ object Applicative {
       f <- ff
     } yield f(a)
   }
+}
+
+trait ApplicativeLaws[F[_]] {
+
+  import Applicative.ops._
+
+  implicit def F: Applicative[F]
+
+  def applicativeIdentity[A](fa: F[A]): Boolean =
+    fa.apply(F.pure((a: A) => a)) == fa
+
+  def applicativeHomomorphism[A, B](a: A, f: A => B): Boolean =
+    F.pure(a).apply(F.pure(f)) == F.pure(f(a))
+
+  def applicativeInterchange[A, B](a: A, ff: F[A => B]): Boolean =
+    F.pure(a).apply(ff) == ff.apply(F.pure((f: A => B) => f(a)))
+
+  def applicativeMap[A, B](fa: F[A], f: A => B): Boolean =
+    fa.map(f) == fa.apply(F.pure(f))
 }
